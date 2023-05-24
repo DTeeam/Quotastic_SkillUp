ef2d1ca5f078b922417d8b2cf260d9c6568687a7
DTeeam <teamdetecnik@gmail.com>
2023-05-24 11:29:58 +0200

    Initial commit

---
+++ frontend/src/components/ui/Layout.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+
+const Layout: FC<Props> = ({ children }) => {
+  return <></>;
+};
+
+export default Layout;
